a8f5219a91e4f1138f0b48d11184b29445eee28a
Kenan Duman <kenanduman1988@gmail.com>
2021-11-16 14:59:58 +0100

    add test controller

---
+++ app/Http/Controllers/BangerGamesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BangerGamesController extends Controller
+{
+    /**
+     * Endpoint: POST /bangergames
+     */
+    public function test()
+    {
+        // TODO: show "gamer" parameter from POST request
+    }
+}
