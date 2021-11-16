2c293277e95b4da7f1ec8be5202bb4d0d7f79210
Kenan Duman <kenanduman1988@gmail.com>
2021-11-16 15:24:25 +0100

    extend test task

--- app/Http/Controllers/BangerGamesController.php
+++ app/Http/Controllers/BangerGamesController.php
@@ -8,6 +8,7 @@ class BangerGamesController extends Controller
 {
     /**
      * Endpoint: POST /bangergames
+     * This endpoint should use BangerGamesMiddleware and check BANGER_GAMES=123qwe on request
      */
     public function test()
     {
