4fbdb9ed876cb9291420ccb0617f90981d43191f
Kenan Duman <kenanduman1988@gmail.com>
2021-11-26 14:55:55 +0300

    add test2

--- app/Http/Controllers/BangerGamesController.php
+++ app/Http/Controllers/BangerGamesController.php
@@ -7,11 +7,32 @@ use Illuminate\Http\Request;
 class BangerGamesController extends Controller
 {
     /**
-     * Endpoint: POST /bangergames
+     * Endpoint: POST /test1
      * This endpoint should use BangerGamesMiddleware and check BANGER_GAMES=123qwe on request
      */
-    public function test()
+    public function test1()
     {
         // TODO: show "gamerId" int parameter from POST request as json response
+    }
+
+    /**
+     * Endpoint: POST /test1
+     * validate brackets in an expression sting like this:
+     * ( 5 * 3 [ 6 ) - 6]
+     *
+     * detect if a string contains any unclosed brackets.
+     * Input: exp = “[()]{}{[()()]()}”
+     * Output: Balanced
+     *
+     * Input: exp = “[(])”
+     * Output: Not Balanced
+     *
+     * {([]){}()} is balanced
+     * {C{}[{[a]}RqhL]{y2}} is balanced
+     * {([]){]()} is not balanced
+     */
+    public function test2()
+    {
+        // TODO: implement logic in new service and response format should be in JSON
     }
 }
